57c3f093d837487301367dd660c63f2c26ffc051
Kenny Na <kennyulna@gmail.com>
2023-12-28 16:38:10 -0700

    script to CLI

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,15 @@
 import bpy
+import sys
+
 # Test
 print("Abstract Art Generator by kennynahh and itsanantk")
 
-# Set the output path for the rendered image
-bpy.context.scene.render.filepath = "//render_output.png"
+# Accept the output path as a command-line argument (this is a CLI interface)
+output_path = sys.argv[-1]
+bpy.context.scene.render.filepath = output_path
 
-# Render the scene
+# render
 bpy.ops.render.render(write_still=True)
+
+# complete
+print("Rendering is complete.")
