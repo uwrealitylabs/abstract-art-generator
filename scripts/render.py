17110de97106007573354f958b613f6cdc0efeca
Kenny Na <kennyulna@gmail.com>
2023-12-28 17:30:21 -0700

    basic render complete

--- scripts/render.py
+++ scripts/render.py
@@ -1,5 +1,13 @@
-import bpy
 import sys
+
+# Add the path to Blender's Python to sys.path
+blender_python_path = "/Applications/Blender.app/Contents/Resources/3.6/python/lib/python3.10"
+sys.path.append(blender_python_path)
+
+# Import bpy
+import bpy
+
+# Now bpy should be recognized
 
 # Test
 print("Abstract Art Generator by kennynahh and itsanantk")
@@ -15,4 +23,5 @@ bpy.ops.render.render(write_still=True)
 print("Rendering is complete.")
 
 # run the CLI script using:
-# blender -b art.blend -P test.py
+# blender -b art.blend -P scripts/render.py
+# this is assuming you have cd'd into the project file directory
