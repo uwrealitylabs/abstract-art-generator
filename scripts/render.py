51187c6fa53371b857f42e14a468b2a9e20f8058
antgenix <antgenix@gmail.com>
2023-12-29 04:37:16 -0800

    Object scalling to proportionality and collision

--- scripts/render.py
+++ scripts/render.py
@@ -2,6 +2,7 @@ import sys
 import os
 import bpy
 import random
+import math
 
 # Detect operating system
 is_windows = sys.platform == "win32"
@@ -31,29 +32,110 @@ bpy.ops.object.select_all(action='DESELECT')
 bpy.ops.object.select_by_type(type='MESH')
 bpy.ops.object.delete()
 
+# Find the outline size of parent class
+def findBoundingBoxDimensions(parent):
+    min_x = 99e99
+    max_x = 0
+    min_y = 99e99
+    max_y = 0
+    min_z = 99e99
+    max_z = 0
+    for child in parent.children:
+        min_x = min(min_x, (child.location.x-child.dimensions.x/2))
+        max_x = max(max_x, (child.location.x+child.dimensions.x/2))
+        min_y = min(min_y, (child.location.y-child.dimensions.z/2))
+        max_y = max(max_y, (child.location.y+child.dimensions.z/2))
+        min_z = min(min_z, (child.location.z-child.dimensions.y/2))
+        max_z = max(max_z, (child.location.z+child.dimensions.y/2))
+    x = max_x-min_x
+    y = max_y-min_y
+    z = max_z-min_z
+    
+    return (x, y, z)
+
+
 # Asset import (all obj files)
 obj_path = "assets/obj"
 obj_files = [f for f in os.listdir(obj_path) if f.endswith('.obj')]
-
+x = 0
 # Set random sizes and positions for imported objects
+old_objects = []
+new_objects = []
+locations = []
+dimensions = []
 for obj_file in obj_files:
+
+    # Debuging purposes only
+    #if x <= 3:
+    #    x += 1
+    #    continue
+
+    old_objects = list(bpy.data.objects)
+
     obj_file_path = os.path.join(obj_path, obj_file)
     bpy.ops.import_scene.obj(filepath=obj_file_path)
+    
+    new_objects = list(set(bpy.data.objects) - set(old_objects))    
+    
+    parent_object = bpy.data.objects.new("GroupParent", None)
 
-    # Get the last imported object (assuming it's the one just imported)
-    imported_object = bpy.context.active_object
+    # Loop through all new objects
+    for imported_object in new_objects:
+        bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='BOUNDS')
+        imported_object.parent = parent_object
 
+    bbd = list(findBoundingBoxDimensions(parent_object))
+
+    # Scale objects to proportioinality
+    #scale_factor = 2 / (bbd[0]+bbd[1]+bbd[2]/3)  # Adjust this value as needed
+    scale_factor = 1 / (max(bbd))  # Adjust this value as needed
+    parent_object.scale = (scale_factor, scale_factor, scale_factor)
+    bbd[0]*=scale_factor
+    bbd[1]*=scale_factor
+    bbd[2]*=scale_factor
+    
+    """
     # Set random scale (size) for the object
     scale_factor = random.uniform(0.1, 1.0)
     imported_object.scale = (scale_factor, scale_factor, scale_factor)
- 
-    # Set random position for the object
-    imported_object.location = (
-        random.uniform(-5, 5),
-        random.uniform(-5, 5),
-        random.uniform(0, 5)
-    )
+    """
 
+    # Set random position for the object
+    location_range = 3
+    colliding = False
+    searching = True
+    while searching:
+        parent_object.location = (
+            random.uniform(-location_range, location_range),
+            random.uniform(-location_range, location_range),
+            random.uniform(-location_range/1.5, location_range/3)
+        )
+        colliding = False
+        for i in range(len(locations)):
+            # If no x collision
+            if parent_object.location.x + bbd[0]/2 >= locations[i][0] - dimensions[i][0]/2 and parent_object.location.x - bbd[0] <= locations[i][0] + dimensions[i][0]/2:
+                    if parent_object.location.z + bbd[1]/2 >= locations[i][2] - dimensions[i][1]/2 and parent_object.location.z - bbd[1]/2 <= locations[i][2] + dimensions[i][1]/2 :
+                        #if parent_object.location.y + bbd[2]/2 >= locations[i][1] - dimensions[i][2]/2 and parent_object.location.y - bbd[2]/2 <= locations[i][1] + dimensions[i][2]/2 :
+                            colliding = True
+
+
+        if not colliding:
+            locations.append(parent_object.location)
+            dimensions.append(bbd)
+            searching = False
+            print("found")
+        
+camera = bpy.data.objects["Camera"]
+camera.location = (0, -10, 3)
+c = (70, 0, 0)
+pi = math.pi
+scene = bpy.data.scenes["Scene"]
+scene.camera.rotation_euler[0] = c[0] * (pi / 180.0)
+scene.camera.rotation_euler[1] = c[1] * (pi / 180.0)
+scene.camera.rotation_euler[2] = c[2] * (pi / 180.0)
+            
+
+bpy.context.active_object.scale= (50, 50, 50)
 # Render
 bpy.ops.render.render(write_still=True)
 
